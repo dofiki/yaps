db24cc62834d2e245b7dfdbba7f9f6f99e3a260d
Dixit Regmi <dixitregmiofficial@gmail.com>
2025-06-18 23:06:10 +0545

    Add user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }],
+    bookmarks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }]
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+export default User;
